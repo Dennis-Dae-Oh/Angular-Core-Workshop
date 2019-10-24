c7d8347a8b1d8c5b790ebf9dde050b4668058bcc
Dennis <dennis@Denniss-MacBook-Air.local>
2019-10-23 20:29:46 -0400

    update

--- Angular/src/app/app.component.spec.ts
+++ Angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'workshop'`, () => {
+  it(`should have as title 'Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('workshop');
+    expect(app.title).toEqual('Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('workshop app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Angular app is running!');
   });
 });
